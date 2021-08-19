8f2aa2e847f961fb6959d6902bcb563c8be6efc0
Szukács Marcell <spiglebach@gmail.com>
2021-08-19 20:35:47 +0200

    build delay and ambience improvements

--- Assets/Tower/Tower.cs
+++ Assets/Tower/Tower.cs
@@ -1,7 +1,23 @@
+using System.Collections;
 using UnityEngine;
 
 public class Tower : MonoBehaviour {
     [SerializeField] private int towerCost = 50;
+    [SerializeField] private float buildTimeInSeconds = 1f;
+
+    private void Start() {
+        foreach (Transform child in transform) {
+            child.gameObject.SetActive(false);
+        }
+        StartCoroutine(Build());
+    }
+
+    private IEnumerator Build() {
+        foreach (Transform child in transform) {
+            child.gameObject.SetActive(true);
+            yield return new WaitForSeconds(buildTimeInSeconds);
+        }
+    }
 
     public bool CreateTowerAt(Tower tower, Vector3 position) {
         var bank = FindObjectOfType<Bank>();
